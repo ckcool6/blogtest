63806eda5d0d2522cdf0e574283921bef68b13fd
rzdtjb <rzdtjb@gmail.com>
2023-09-03 01:17:54 +0800

    first commit

--- Frame/Vendor/Smarty.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Frame\Vendor;
-//载入原始的Smarty类文件
-require_once(FRAME_PATH."Vendor".DS."Smarty".DS."libs".DS."Smarty.class.php");
-//定义Smarty类，并继承原始的Smarty类
-final class Smarty extends \Smarty{
-
-}
-?>
