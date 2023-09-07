c4673b59baa4459e9f781cab60e0f43d9f982196
rzdtjb <rzdtjb@gmail.com>
2023-09-08 02:27:49 +0800

    register

--- Admin/Controller/UserController.class.php
+++ Admin/Controller/UserController.class.php
@@ -28,4 +28,38 @@ class UserController extends BaseController
         }
     }
 
+    public function add()
+    {
+        $this->smarty->display("./User/add.html");
+    }
+
+    //register
+    public function insert()
+    {
+        //获取表单数据
+        $data['username'] = $_POST['username'];
+        $data['password'] = md5($_POST['password']);
+        $data['name'] = $_POST['name'];
+        $data['tel'] = $_POST['tel'];
+        $data['status'] = $_POST['status'];
+        $data['role'] = $_POST['role'];
+        $data['addate'] = time();
+
+        //是否重复注册
+        if (UserModel::getInstance()->rowCount("username='{$data['username']}'")) {
+            $this->jump("用户名{$data['username']}已注册", "?c=User");
+        }
+
+        //判断两次输入密码是否一致
+        if ($data['password'] != md5($_POST['confirmpwd'])) {
+            $this->jump("两次输入密码不一致", "?c=User");
+        }
+
+        if (UserModel::getInstance()->insert($data)) {
+            $this->jump("注册成功", "?c=User");
+        } else {
+            $this->jump("注册失败", "?c=User");
+        }
+    }
+
 }
