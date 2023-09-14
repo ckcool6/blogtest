bcb954cf3cf7ea94b0ff1fb5792e4c485a2eb83f
rzdtjb <rzdtjb@gmail.com>
2023-09-15 01:51:21 +0800

    article index

--- Admin/Controller/ArticleController.class.php
+++ Admin/Controller/ArticleController.class.php
@@ -2,7 +2,33 @@
 
 namespace Admin\Controller;
 
-class ArticleController
-{
+use Admin\Model\ArticleModel;
+use Admin\Model\CategoryModel;
+use Frame\Libs\BaseController;
 
+class ArticleController extends BaseController
+{
+    public function index()
+    {
+        $categorys = CategoryModel::getInstance()->categoryList(CategoryModel::getInstance()->fetchAll("id ASC"));
+        $articles = ArticleModel::getInstance()->fetchAllWithJoin();
+
+        $this->smarty->assign(array(
+            'categorys' => $categorys,
+            'articles' => $articles,
+        ));
+
+        $this->smarty->display("./Article/index.html");
+
+    }
+
+    public function add()
+    {
+        //todo
+    }
+
+    public function insert()
+    {
+        //todo
+    }
 }
--- Admin/Model/ArticleModel.class.php
+++ Admin/Model/ArticleModel.class.php
@@ -2,7 +2,22 @@
 
 namespace Admin\Model;
 
-class ArticleModel
-{
+use Frame\Libs\BaseModel;
 
+class ArticleModel extends BaseModel
+{
+    protected $table = "article";
+
+    public function fetchAllWithJoin()
+    {
+        //构建连表查询的sql语句
+        $sql = "select article.*,category.classname,user.username from {$this->table} "; //加空格
+        $sql .= "left join `category` on article.category_id=category.id ";//加空格
+        $sql .= "left join `user` on article.user_id=user.id "; //加空格
+        $sql .= "order by article.orderby ASC,article.id DESC";
+
+        //返回二维数组
+        return $this->pdo->fetchAll($sql);
+
+    }
 }
